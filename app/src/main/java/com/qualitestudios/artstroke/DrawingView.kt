17a49fe66af41864e38e159b89c79ea5337113a3
Chethan Bhandarkar <b.chethanb91@gmail.com>
2020-05-19 22:57:44 +0530

    Version 0.2 ArtStroke basic done working

--- app/src/main/java/com/qualitestudios/artstroke/DrawingView.kt
+++ app/src/main/java/com/qualitestudios/artstroke/DrawingView.kt
@@ -3,6 +3,7 @@ package com.qualitestudios.artstroke
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 //creating a view
 class DrawingView(context: Context, attrs: AttributeSet) : View(context,attrs){
@@ -16,6 +17,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context,attrs){
     private var mCanvasPaint:Paint?=null
     private var mBrushSize:Float=0.toFloat()
     private var color= Color.BLACK
+    private val mPaths=ArrayList<CustomPath>()
 
     //canvastodraw
     private var canvas:Canvas?=null
@@ -31,15 +33,82 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context,attrs){
         mDrawPaint!!.style=Paint.Style.STROKE
         mDrawPaint!!.strokeJoin=Paint.Join.ROUND
         mDrawPaint!!.strokeCap=Paint.Cap.ROUND
+        mCanvasPaint=Paint(Paint.DITHER_FLAG)
+        mBrushSize=20.toFloat()
 
 
 
 
     }
+// called wen size of our screen chnges
+    //we are creating canvas ech time size changed
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        //set canvas
+        mCanvasBitmap= Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
+        canvas=Canvas(mCanvasBitmap!!)
+
+    }
+//change canvas to canvas? if fails
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!,0f,0f,mCanvasPaint)
 
 
+    for(path in mPaths)
+    {
+        mDrawPaint!!.strokeWidth=path.brushThickness
+        mDrawPaint!!.color=path.color
+        canvas.drawPath(path, mDrawPaint!!)
+    }
 
 
+    if(!mDrawPath!!.isEmpty) {
+mDrawPaint!!.strokeWidth=mDrawPath!!.brushThickness
+        mDrawPaint!!.color=mDrawPaint!!.color
+
+        canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+    }
+    }
+//motion event
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+      val touchX=event?.x
+        val touchY=event?.y
+        when(event?.action)
+        {
+            MotionEvent.ACTION_DOWN->{
+                mDrawPath!!.color=color
+                mDrawPath!!.brushThickness=mBrushSize
+                mDrawPath!!.reset()
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.moveTo(touchX,touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_MOVE->{
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.lineTo(touchX,touchY)
+                    }
+                }
+
+
+            }
+            MotionEvent.ACTION_UP->{
+                mPaths.add(mDrawPath!!)
+                mDrawPath=CustomPath(color,mBrushSize)
+
+            }
+            else->return false
+
+        }
+
+invalidate()
+
+        return true
+    }
 
     //only be usable within drawing view nd use variable
     //custompath inherits from path
--- app/src/main/java/com/qualitestudios/artstroke/MainActivity.kt
+++ app/src/main/java/com/qualitestudios/artstroke/MainActivity.kt
@@ -2,11 +2,17 @@ package com.qualitestudios.artstroke
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
